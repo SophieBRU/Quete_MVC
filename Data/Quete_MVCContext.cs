64c9072bc7a4974772bfa9304c55bd4b240c6801
Sophie_BRULTET <sophie.brultet@gmail.com>
2020-11-28 13:29:46 +0100

    Quete MVC

---
+++ Data/Quete_MVCContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Quete_MVC.Models;
+
+namespace Quete_MVC.Data
+{
+    public class Quete_MVCContext : DbContext
+    {
+        public Quete_MVCContext(DbContextOptions<Quete_MVCContext> options)
+                : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+    }
+}
